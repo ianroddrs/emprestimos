e7d3221efc4d5520f018f6bd8aeff5fe010c5bbc
Kevin Washington <washingtonkevin703@gmail.com>
2025-01-09 10:10:29 -0300

    ajusta taxa de juros e formatação da simulação de pagamentos

--- apps/main/views.py
+++ apps/main/views.py
@@ -8,8 +8,8 @@ def photo_wall(request):
 def emp():
   # Dados iniciais
   principal = 20000  # valor do empréstimo inicial
-  juros_mensal = 0.02  # taxa de juros (2% ao mês)
-  pagamento = 700  # pagamento nos meses subsequentes
+  juros_mensal = 0.0165 # taxa de juros (2% ao mês)
+  pagamentos = [700]  # pagamento nos meses subsequentes
 
   # Simulação dos pagamentos
   saldo = principal  # saldo inicial é igual ao valor emprestado
@@ -17,13 +17,20 @@ def emp():
   meses = 0
 
   # Agora, simular os pagamentos de 700 reais até quitar o saldo
-  while saldo > 0:
+  for pagamento in pagamentos:
+      valor_juros = saldo * juros_mensal
+      if pagamento > saldo + valor_juros:
+          pagamento = saldo + valor_juros
       saldo = saldo * (1 + juros_mensal) - pagamento
       total_pago += pagamento
       meses += 1
 
-  # Se o saldo ficou negativo (pagamento maior que a dívida), ajustar o valor pago
-  if saldo < 0:
-      total_pago += saldo  # subtrair o excesso pago do total
-
-  print(meses, total_pago)
+      texto = f"""
+          mes: {meses}
+          valor_emprestado: ${principal:.2f}
+          pagamento: ${pagamento:.2f}
+          juros (1,65%): ${valor_juros:.2f}
+          saldo restante: ${saldo:.2f}
+          total pago: ${total_pago:.2f}
+      """
+      print(texto)
