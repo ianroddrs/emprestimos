c456572014777b6e27e8bbb2ed4dd8ee0be8e32c
Kevin Washington <washingtonkevin703@gmail.com>
2025-01-09 12:49:31 -0300

    atualiza o caminho do STATIC_ROOT para usar a sintaxe do Path

--- core/settings.py
+++ core/settings.py
@@ -112,7 +112,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'core/staticfiles')
+STATIC_ROOT = BASE_DIR / 'core/staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'assets'
 ]
