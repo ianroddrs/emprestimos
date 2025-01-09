daef060ffdbe479834315aed80f3428f45bed020
Kevin Washington <washingtonkevin703@gmail.com>
2025-01-09 12:35:58 -0300

    adiciona configuração para servir arquivos estáticos em produção

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
   path('admin/', admin.site.urls),
   path('', include('apps.main.urls')),
 ]
+
+if settings.DEBUG is False:  # Serve estáticos somente em produção
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
